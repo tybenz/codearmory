03242b9b4ce458c485862443dfcfe9afa0077d96
tybenz <tabenziger@gmail.com>
2012-09-17 22:11:21 -0700

    heroku fix

--- config/initializers/0_load_app_configs.rb
+++ config/initializers/0_load_app_configs.rb
@@ -1,1 +1,3 @@
-CONFIG = YAML.load(ERB.new(IO.read("#{Rails.root.to_s}/config/codearmory.yml")).result)[Rails.env].symbolize_keys
+require 'yaml'
+
+CONFIG = YAML.load(ERB.new(IO.read("#{Rails.root.to_s}/config/codearmory.yaml")).result)[Rails.env].symbolize_keys
